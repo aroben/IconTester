804fc6a00d8b1445c67842b8ecb36d68c803ee86
Adam Roben <adam@roben.org>
2013-05-03 12:44:00 -0400

    Allow dragging images onto the window

--- IconTester/AppDelegate.m
+++ IconTester/AppDelegate.m
@@ -12,7 +12,41 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-  // Insert code here to initialize your application
+  NSArray *types = [@[NSURLPboardType] arrayByAddingObjectsFromArray:NSImage.imagePasteboardTypes];
+  [self.window registerForDraggedTypes:types];
+}
+
+- (void)setIcon:(NSImage *)icon
+{
+  [NSApp setApplicationIconImage:icon];
+}
+
+#pragma mark - NSDraggingDestination
+
+- (NSDragOperation)draggingEntered:(id < NSDraggingInfo >)sender
+{
+  return NSDragOperationCopy;
+}
+
+- (NSDragOperation)draggingUpdated:(id < NSDraggingInfo >)sender
+{
+  return NSDragOperationCopy;
+}
+
+- (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender
+{
+  return YES;
+}
+
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
+{
+  NSImage *image = [[NSImage alloc] initWithPasteboard:sender.draggingPasteboard];
+  if (!image)
+    image = [[NSImage alloc] initWithContentsOfURL:[NSURL URLFromPasteboard:sender.draggingPasteboard]];
+  if (!image)
+    return NO;
+  [self setIcon:image];
+  return YES;
 }
 
 @end
